286c539c4070d5a4289b3b11c46f2199fed08e1c
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-10-20 22:47:04 +0100

    Adding in the start of some menu code

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+// $("#toggle").click(
+// function() {
+//         $(this).toggleClass("on");
+//         $("#menu").slideToggle();
+//         console.log("yee");
+// });
+
+// $("#name").click(function() {
+//         console.log("yee");
+// });
+// Learn Javascript
+var x = document.getElementById("toggle")
+
+function toggleClass() {
+    if (x == true)
+        console.log("yee");
+};
+
