2be3d49b072e0a7ade55668d0db38c5936564cf3
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-10-20 23:05:29 +0100

    Adding in the groundwork

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,4 @@
-// $("#toggle").click(
-// function() {
-//         $(this).toggleClass("on");
-//         $("#menu").slideToggle();
-//         console.log("yee");
-// });
-
-// $("#name").click(function() {
-//         console.log("yee");
-// });
-// Learn Javascript
-var x = document.getElementById("toggle")
-
-function toggleClass() {
-    if (x == true)
-        console.log("yee");
-};
-
+$("#toggle").click(function() {
+  $(this).toggleClass("on");
+  $("#menu").slideToggle();
+});
