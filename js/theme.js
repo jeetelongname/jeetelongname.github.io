f014c8cd173e7133ebd321a5defd7133947cb31a
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2020-10-26 00:13:59 +0000

    adding in a theme switcher

---
+++ js/theme.js
@@ -0,0 +1,28 @@
+// this one is jut to wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+
+    const themeStylesheet = document.getElementById('theme');
+    const storedTheme = localStorage.getItem('theme');
+    // const storedIcon  = localStorage.getItem('theme-toggle')
+    if(storedTheme){
+        themeStylesheet.href = storedTheme;
+    }
+    // if(storedIcon){
+    //     themeToggle.innerHTML= storedIcon
+    // }
+    const themeToggle = document.getElementById('theme-toggle');
+    themeToggle.addEventListener('click', () => {
+        // if it's light -> go dark
+        if(themeStylesheet.href.includes('light')){
+            themeStylesheet.href = 'css/dark.css';
+            themeToggle.innerHTML= '<i class="far fa-sun"></i>';
+        } else {
+            // if it's dark -> go light
+            themeStylesheet.href = 'css/light.css';
+            themeToggle.innerHTML = '<i class="far fa-moon"></i>';
+        }
+        // save the preference to localStorage
+        localStorage.setItem('theme',themeStylesheet.href)  
+        // localStorage.setItem('theme-toggle',themeToggle.innerHTML)  
+    })
+})
