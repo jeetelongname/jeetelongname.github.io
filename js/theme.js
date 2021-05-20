add27fc4c821b37613d27491f0c54c13a224ae59
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-05-20 23:22:08 +0100

    fix: switching to lucide icons

--- js/theme.js
+++ js/theme.js
@@ -1,47 +1,47 @@
 // this one is jut to wait for the page to load
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const themeStylesheet = document.getElementById("theme");
+  const themeToggle = document.getElementById("theme-toggle");
+  const storedTheme = localStorage.getItem("theme");
+  var prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    const themeStylesheet = document.getElementById('theme');
-    const themeToggle = document.getElementById('theme-toggle');
-    const storedTheme = localStorage.getItem('theme');
-    var prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    
-
-    if (storedTheme) {
-        if (themeStylesheet.href.includes('light')) {
-            // if it's light -> go dark
-            dark(themeStylesheet, themeToggle)
-        } else {
-            // if it's dark -> go light
-            light(themeStylesheet, themeToggle)
-        }
-        localStorage.setItem('theme', themeStylesheet.href)
+  if (storedTheme) {
+    if (themeStylesheet.href.includes("light")) {
+      // if it's light -> go dark
+      dark(themeStylesheet, themeToggle);
+    } else {
+      // if it's dark -> go light
+      light(themeStylesheet, themeToggle);
     }
+    localStorage.setItem("theme", themeStylesheet.href);
+  }
 
-    if (prefersDark) {
-        dark(themeStylesheet, themeToggle)
+  if (prefersDark) {
+    dark(themeStylesheet, themeToggle);
+  }
+
+  themeToggle.addEventListener("click", () => {
+    if (themeStylesheet.href.includes("light")) {
+      // if it's light -> go dark
+      dark(themeStylesheet, themeToggle);
+    } else {
+      // if it's dark -> go light
+      light(themeStylesheet, themeToggle);
     }
-
-    themeToggle.addEventListener('click', () => {
-        if (themeStylesheet.href.includes('light')) {
-            // if it's light -> go dark
-            dark(themeStylesheet, themeToggle)
-        } else {
-            // if it's dark -> go light
-            light(themeStylesheet, themeToggle)
-        }
-        // save the preference to localStorage
-    })
-})
+    // save the preference to localStorage
+  });
+});
 function dark(themeStylesheet, themeToggle) {
-    themeStylesheet.href = 'css/dark.css';
-    themeToggle.innerHTML = '<i data-feather="sun" alt="light" title="Switch to light mode"></i>';
-    feather.replace()
-    localStorage.setItem('theme', themeStylesheet.href)
+  themeStylesheet.href = "css/dark.css";
+  themeToggle.innerHTML =
+    '<i icon-name="sun" alt="light" title="Switch to light mode"></i>';
+  lucide.createIcons();
+  localStorage.setItem("theme", themeStylesheet.href);
 }
 function light(themeStylesheet, themeToggle) {
-    themeStylesheet.href = 'css/light.css';
-    themeToggle.innerHTML ='<i data-feather="moon" alt="Dark" title="Switch to dark mode"></i>';
-    feather.replace()
-    localStorage.setItem('theme', themeStylesheet.href)
+  themeStylesheet.href = "css/light.css";
+  themeToggle.innerHTML =
+    '<i icon-name="moon" alt="Dark" title="Switch to dark mode"></i>';
+  lucide.createIcons();
+  localStorage.setItem("theme", themeStylesheet.href);
 }
