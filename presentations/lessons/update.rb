e894d821e08c36ca4934a620d23967d11c46ff18
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-03-10 20:09:44 +0000

    add index generator

---
+++ presentations/lessons/update.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+#
+# frozen_string_literal: true
+
+require 'erb'
+
+lessons = `ls`.split("\n").filter_map do |file|
+  name, type = file.split('.')
+
+  [name, type] if %w[html pdf].include?(type) && name != 'index'
+end
+
+template = <<~HEAD
+  <html class="no-js" lang="">
+  <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Lessons</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+  </head>
+  <body>
+  <ul>
+  % lessons.each do |lesson|
+          <li> <a href="<%= lesson.join(".") %>"> <%= lesson[0].gsub("-", " ") %> </a> </li>
+  % end
+  </ul>
+  </body>
+  <html>
+HEAD
+
+html = ERB.new(template, trim_mode: '%<>')
+File.write('index.html', html.result)
