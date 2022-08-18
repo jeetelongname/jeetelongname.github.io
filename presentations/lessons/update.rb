9d3b0636e15b5d1c06b1179c54ff892ec8016f57
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-08-18 03:07:24 +0100

    cleanup update

--- presentations/lessons/update.rb
+++ presentations/lessons/update.rb
@@ -10,30 +10,31 @@ lessons = `ls`.split("\n").filter_map do |file|
   [name, type] if %w[html pdf].include?(type) && name != 'index'
 end
 
-template = <<~HEAD
-  <html class="no-js" lang="">
-  <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Lessons</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+html = ERB.new(DATA.read, trim_mode: '%<>')
+File.write('index.html', html.result)
 
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
-        <!-- <link rel="stylesheet" href="../../css/presentation.css" type=
-                    "text/css" media="screen"> -->
+__END__
+
+<html class="no-js" lang="">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Lessons</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+      <!-- Place favicon.ico in the root directory -->
+      <!-- <link rel="stylesheet" href="../../css/presentation.css" type=
+                  "text/css" media="screen"> -->
 
   </head>
   <body>
-  <h1> Lessons </h1>
-  <ul>
-  % lessons.each do |lesson|
-          <li> <a href="<%= lesson.join(".") %>"> <%= lesson[0].gsub("-", " ") %> </a> </li>
-  % end
-  </ul>  </body>
-  <html>
-HEAD
-
-html = ERB.new(template, trim_mode: '%<>')
-File.write('index.html', html.result)
+    <h1> Lessons </h1>
+    <ul>
+% lessons.each do |lesson|
+        <li> <a href="<%= lesson.join(".") %>"> <%= lesson[0].gsub("-", " ") %> </a> </li>
+% end
+    </ul>
+  </body>
+<html>
